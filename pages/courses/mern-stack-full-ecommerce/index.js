13de242f6ba371ba879c99a63fb3815e6bc2feaf
Mahbub Hasan <99654070+mahbubhswe@users.noreply.github.com>
2022-07-23 13:37:41 +0600

    Update index.js

--- pages/courses/mern-stack-full-ecommerce/index.js
+++ pages/courses/mern-stack-full-ecommerce/index.js
@@ -126,6 +126,9 @@ export default function index() {
             মেসেঙ্গারে আমাকে জিজ্ঞাসা করুন — ধন্যবাদ
           </Alert>
         </Stack>
+<div style="textAlign:center">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/tULVlvu40H4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
       </Container>
     </Layout>
   );
